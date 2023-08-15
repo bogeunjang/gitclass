baf57f7c7b147e27539ead948a317b836597000a
spritte <elicespritte@gmail.com>
2023-08-15 14:39:12 +0900

    commit

---
+++ compare.js
@@ -0,0 +1,5 @@
+let age = 25;
+
+console.log(age == '25');
+console.log(age === '25');
+console.log(age === 25);
---
+++ script.js
@@ -0,0 +1,9 @@
+let year = 2023;
+const today = '광복절';
+const pass = false;
+const sale = '7';
+
+let stringresult = Number(point) * String(year);
+let result = Number(point) * Number(sale);
+
+console.log(typeof(stringresult));
